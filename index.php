9a45828d39bfa4caa0ab9092158a1fb821d4f75f
gerardduenas <gerardduenas@users.noreply.github.com>
2018-05-29 09:18:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a Wordpress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specigic matches a query.
+ * 
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ * 
+ * @package Wordpress
+ * @subpackage Zaura
+ * @since Zaura 1.0
+ */
+
+get_header(); //Include the header.php template ?>
+
+    <div id="primary"
